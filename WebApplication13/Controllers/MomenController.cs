35f9fffb5c6f522b16287acc15f922441d6d3a43
Momen-AlAkhras <momen.alakhras03@gmail.com>
2023-01-31 16:40:36 +0300

    m

---
+++ WebApplication13/Controllers/MomenController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication13.Controllers
+{
+    public class MomenController : Controller
+    {
+        // GET: Momen
+        public string  Name()
+        {
+            return "Name";
+        }
+        public string Age()
+        {
+            return "18";
+
+        }
+        public string Hobbies()
+        {
+            return "gaming";
+        }
+        public string Position()
+        {
+            return "Software Devloper";
+        }
+    }
+}
